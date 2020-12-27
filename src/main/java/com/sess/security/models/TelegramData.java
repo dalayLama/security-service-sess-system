32018254c74a3d5bd535373ab747cd261b5b05cb
Artur Skorik <admskorik@gmail.com>
2020-12-27 20:27:16 +1000

    added jpa annotations for entities

--- src/main/java/com/sess/security/models/TelegramData.java
+++ src/main/java/com/sess/security/models/TelegramData.java
@@ -2,16 +2,34 @@ package com.sess.security.models;
 
 import com.sess.security.models.user.SecurityUser;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "telegram_data")
 public class TelegramData {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "telegram_data_id_seq"
+    )
+    @SequenceGenerator(
+            name = "telegram_data_id_seq",
+            sequenceName = "telegram_data_id_seq",
+            allocationSize = 1
+    )
     private Long id;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_user", unique = true, nullable = false, updatable = false)
     private SecurityUser user;
 
-    private String key;
+    @Column(name = "telegram_id", unique = true, updatable = false)
+    private Integer key;
 
     public Long getId() {
         return id;
+
     }
 
     public void setId(Long id) {
@@ -26,11 +44,11 @@ public class TelegramData {
         this.user = user;
     }
 
-    public String getKey() {
+    public Integer getKey() {
         return key;
     }
 
-    public void setKey(String key) {
+    public void setKey(Integer key) {
         this.key = key;
     }
 }
