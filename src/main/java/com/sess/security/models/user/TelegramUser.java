c69f2f45b8f35d997d01b0c38ee3127260092f35
Artur Skorik <admskorik@gmail.com>
2020-12-29 02:37:35 +1000

    added json annotations for telegram user

--- src/main/java/com/sess/security/models/user/TelegramUser.java
+++ src/main/java/com/sess/security/models/user/TelegramUser.java
@@ -1,5 +1,7 @@
 package com.sess.security.models.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sess.security.models.City;
 import com.sess.security.models.Sex;
 
@@ -14,7 +16,14 @@ public class TelegramUser extends BaseUser {
     @Min(value = 1, message = "Идентификатор telegram-а не может быть меньше 0")
     private final int telegramId;
 
-    public TelegramUser(String nickname, String email, City city, Sex sex, LocalDateTime birthday, int telegramId) {
+    @JsonCreator
+    public TelegramUser(
+            @JsonProperty(value = "nickname") String nickname,
+            @JsonProperty(value = "email") String email,
+            @JsonProperty(value = "city") City city,
+            @JsonProperty(value = "sex") Sex sex,
+            @JsonProperty(value = "birthday") LocalDateTime birthday,
+            @JsonProperty(value = "telegramId") int telegramId) {
         super(nickname, email, city, sex, birthday);
         this.telegramId = telegramId;
     }
